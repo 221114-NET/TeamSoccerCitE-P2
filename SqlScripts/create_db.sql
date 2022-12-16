c0919257fa26527504b0deb7859f86561cb88da3
JasonTejadaRevature <jason263@revature.net>
2022-12-15 21:17:09 -0800

    Created tables for the customers and products

--- SqlScripts/create_db.sql
+++ SqlScripts/create_db.sql
@@ -1,7 +1,30 @@
 -- Based on the ERD for the Database, we made these SQL scripts
--- TODO create table for Customer
--- TODO create table for Product
--- TODO create table for ProductCategory
+-- Table for Customers
+CREATE TABLE Customer(
+    CustomerId VARCHAR(36) PRIMARY KEY,
+    Email VARCHAR(50) NOT NULL UNIQUE,
+    Username VARCHAR(50) NOT NULL UNIQUE,
+    Password VARCHAR(50) NOT NULL,
+    ProfilePic VARBINARY(MAX)
+);
+
+-- Table for Products
+CREATE TABLE Product(
+    ProductId VARCHAR(36) PRIMARY KEY,
+    Name VARCHAR(50) NOT NULL,
+    Description VARCHAR(255) NOT NULL,
+    Price FLOAT(53) NOT NULL,
+    Quantity INT NOT NULL,
+    CategoryId INT NOT NULL,
+    ImageLocation VARCHAR(50) NULL
+);
+
+-- Table for ProductCategories
+CREATE TABLE ProductCategory(
+    CategoryId INT PRIMARY KEY,
+    CategoryName VARCHAR(50) NOT NULL
+);
+
 -- TODO create table for Customer Session
 -- TODO create table for Cart Items
 -- TODO create table for Order Details
