a8d657e65defcfe35bcb89c2c9d0ceb9ef1b7e9a
JasonTejadaRevature <jason263@revature.net>
2022-12-17 13:47:31 -0800

    Finished first version of user registration in the backend

--- SoccerCitE_API/DataLayer/CustomerData.cs
+++ SoccerCitE_API/DataLayer/CustomerData.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 using ModelLayer;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
 
 namespace DataLayer
 {
@@ -16,9 +17,52 @@ namespace DataLayer
             this._connectionString = new ConfigurationBuilder().AddJsonFile("appsettings.Development.json").Build().GetSection("ConnectionStrings")["SoccerCitEDB"];
         }
 
-        public async Task<Customer> PostCustomer(Customer c) {
-            Console.WriteLine(_connectionString);
-            return null!;
+        public async Task<Customer> PostCustomer(Customer newCustomer)
+        {
+            //List<Customer> list = new List<Customer>();
+
+            using SqlConnection connection = new SqlConnection(_connectionString);
+            await connection.OpenAsync();
+            string cmdText = "INSERT INTO Customer (email, username, password, profilePic)" +
+                             "VALUES (@email, @username, @password, NULL);" +
+                             "SELECT customerId, email, username, password, profilePic FROM Customer WHERE email = @customerEmail;";
+            using SqlCommand cmd = new SqlCommand(cmdText, connection);
+
+            cmd.Parameters.AddWithValue("@email", newCustomer.Email);
+            cmd.Parameters.AddWithValue("@username", newCustomer.Username);
+            cmd.Parameters.AddWithValue("@password", newCustomer.Password);
+            cmd.Parameters.AddWithValue("@customerEmail", newCustomer.Email);
+            //cmd.Parameters.AddWithValue("@profilePic", newCustomer.ProfilePic);
+
+            using SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+            /*while (await reader.ReadAsync())
+            {
+                int id = reader.GetInt32(0);
+                string email = reader.GetString(1);
+                string username = reader.GetString(2);
+                string password = reader.GetString(3);
+                //int profilePic = reader.GetInt32(4);
+
+                Customer tmp = new Customer(email, username, password); //, profilePic
+                //list.Add(tmp);
+            }
+            */
+            await reader.ReadAsync();
+            int id = reader.GetInt32(0);
+            string email = reader.GetString(1);
+            string username = reader.GetString(2);
+            string password = reader.GetString(3);
+
+            await connection.CloseAsync();
+            //logger.LogInformation("Executed AddCustomer");
+
+            //return list;
+            //Customer registeredCustomer = new Customer(email, username, password);
+            //return registeredCustomer;
+            
+            //This does the same thing
+            return new Customer(email, username, password);
         }
     }
 }
